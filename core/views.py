d0e99edd0663cd401531edcbc219b1c0bd517690
GustavoBurnsSoares <gburnssoares@gmail.com>
2021-03-11 14:56:24 -0300

    Adição de filtros em eventos para os recentes e os que aindão estao por vir

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
@@ -43,7 +45,9 @@ def retorna_local(request, titulo_evento):
 @login_required(login_url='/login')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__gt=data_atual)
     response = {'eventos': evento}
     return render(request, 'agenda.html', response)
 
@@ -82,10 +86,20 @@ def submit_evento(request):
 
 @login_required(login_url='/login')
 def delete_evento(request, id_evento):
-    evento = Evento.objects.get(id=id_evento)
     usuario = request.user
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
     if usuario == evento.usuario:
         evento.delete()
     else:
-        pass
+        raise Http404()
     return redirect('/')
+
+
+@login_required(login_url='/login')
+def json_lista_evento(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
